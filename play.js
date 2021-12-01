e38170396e6504fd397ac6427e2d7c4c5bf59d5b
Vlad-Lab319 <solo15mag@gmail.com>
2021-12-01 23:08:58 +0000

    handleUserInput fixed

--- play.js
+++ play.js
@@ -4,19 +4,21 @@ const connect = require('./client');
 console.log("Connecting ...");
 connect();
 
-const setupInput = function() {
+const setupInput = function(handleUserInput) {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", handleUserInput(data));
+  stdin.on("data", handleUserInput);
   return stdin;
 
 };
 
-const handleUserInput = (key) => {
+const handleUserInput = function (key) {
   
   if (key === '\u0003') {
     process.exit();
   }
 }
+
+setupInput(handleUserInput);
